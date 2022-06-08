d0af3736941f33fc2ee2b86e2ed8a19f538f1a8e
Joao_Godinho <git.qodeless@outlook.com>
2022-06-08 17:12:29 -0300

    Completando Desafio 05 - Salvando dados em arquivo CSV

---
+++ Desafio_05/src/main/java/Class/Student.java
@@ -0,0 +1,13 @@
+package Class;
+
+public class Student {
+    String Name;
+    int Age;
+    int Grade;
+    String filepath = "listaAlunos.txt";
+
+    public String ToString() {
+        return "Name: " + Name + " Age; " + Age +" Grade: " + Grade;
+    }
+}
+
